649e6a0f441d3cb47b1018170b9f0ffcc7b6508a
kyy0x4 <kyyaspi13@gmail.com>
2026-08-26 22:26:22 +0700

    eco driving map: ganti tile carto ke openstreetmap dengan kanvas light gray via css filter

--- src/pages/EcoDrivingPage.tsx
+++ src/pages/EcoDrivingPage.tsx
@@ -981,8 +981,9 @@ export default function EcoDrivingPage({ isTAM = false }: { isTAM?: boolean }) {
                     zoomControl={false}
                   >
                     <TileLayer
-                      url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-                      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                      className="osm-light"
+                      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     />
                     <MapUpdater violations={activeViolations} />
                     
